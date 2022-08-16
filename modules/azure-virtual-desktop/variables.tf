77b7f156685c8f7aecca8aa3212a9939080b3af8
amai-be <alessio.maiorano@bechtle.com>
2022-08-16 20:25:02 +0200

    added variable declarations to variables.tf

--- modules/azure-virtual-desktop/variables.tf
+++ modules/azure-virtual-desktop/variables.tf
@@ -6,6 +6,33 @@ variable "environment" {
 
 }
 
+variable "start_vm_on_connect" {
+
+}
+
+variable "type" {
+
+}
+
+variable "load_balancer_type" {
+
+}
+
+variable "custom_rdp_properties" {
+  
+}
+
+variable "description" {
+  
+}
+
+variable "personal_desktop_assignment_type" {
+  
+}
+variable "maximum_sessions_allowed" {
+  
+}
+
 variable "global_settings" {
   default = {
     location = "westeurope"
