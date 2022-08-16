36198e1a843565aae3ffae1ecdd806f027681eb4
amai-be <alessio.maiorano@bechtle.com>
2022-08-16 20:31:40 +0200

    edited variables default values

--- modules/azure-virtual-desktop/variables.tf
+++ modules/azure-virtual-desktop/variables.tf
@@ -1,36 +1,45 @@
 variable "workload" {
-
+  type = "string"
+  default = null
 }
 
 variable "environment" {
-
+  type = "string"
+  default = null
 }
 
 variable "start_vm_on_connect" {
-
+  type = "bool"
+  default = null
 }
 
 variable "type" {
-
+  type = "string"
+  default = null
 }
 
 variable "load_balancer_type" {
-
+  type = "string"
+  default = null
 }
 
 variable "custom_rdp_properties" {
-  
+  type = "string"
+  default = null
 }
 
 variable "description" {
-  
+  type = "string"
+  default = null
 }
 
 variable "personal_desktop_assignment_type" {
-  
+  type = "string"
+  default = null
 }
 variable "maximum_sessions_allowed" {
-  
+  type = "number"
+  default = null
 }
 
 variable "global_settings" {
