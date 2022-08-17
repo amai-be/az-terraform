d1e73c1e80b2647454c207a190e4fe8cd27589d4
amai-be <alessio.maiorano@bechtle.com>
2022-08-17 15:04:19 +0200

    added variable for global settings

--- modules/azure-virtual-desktop/variables.tf
+++ modules/azure-virtual-desktop/variables.tf
@@ -1,3 +1,8 @@
+variable "global_settings" {
+    description = "Configuration object - Global Settings"
+    default = {}
+}
+
 variable "avd" {
     description = "Configuration object - Azure Virtual Desktop resources"
     default = {}
