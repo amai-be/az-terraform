4de94b464a5a28f1b3b1026bd8e9b9e6214c4452
amai-be <alessio.maiorano@bechtle.com>
2022-08-17 15:08:26 +0200

    changed personal desktop assignment type default

--- modules/azure-virtual-desktop/azure-virtual-desktop.tf
+++ modules/azure-virtual-desktop/azure-virtual-desktop.tf
@@ -11,6 +11,6 @@ module "avd-environment" {
     load_balancer_type = can(each.value.load_balancer_type) ? each.value.load_balancer_type : "DepthFirst"
     custom_rdp_properties = can(each.value.custom_rdp_properties) ? each.value.custom_rdp_properties : "drivestoredirect:s:;audiomode:i:0;videoplaybackmode:i:1;redirectclipboard:i:1;redirectprinters:i:1;devicestoredirect:s:*;redirectcomports:i:1;redirectsmartcards:i:0;usbdevicestoredirect:s:*;enablecredsspsupport:i:1;use multimon:i:1;audiocapturemode:i:1"
     description = can(each.value.description) ? each.value.description : "Test"
-    personal_desktop_assignment_type = can(each.value.personal_desktop_assignment_type, "Automatic")
+    personal_desktop_assignment_type = can(each.value.personal_desktop_assignment_type, null)
     maximum_sessions_allowed = can(each.value.maximum_sessions_allowed) ? each.value.maximum_sessions_allowed : 8
 }
