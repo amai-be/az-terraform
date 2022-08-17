87d41bc05c19f73487c5574179565f7a51961f30
amai-be <alessio.maiorano@bechtle.com>
2022-08-17 15:02:46 +0200

    changed provider configuration to main module

--- modules/azure-virtual-desktop/modules/environment/main.tf
+++ modules/azure-virtual-desktop/modules/environment/main.tf
@@ -1,18 +1,3 @@
-# Azure Provider source and version being used
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "= 3.0.0"
-    }
-  }
-}
-
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-}
-
 locals {
     hp_prefix = "vdpool"
     ws_prefix = "vdws"
